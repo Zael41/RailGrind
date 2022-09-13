7eca08199a5cee7ce87a6fcd2093b473cc398df5
Zael41 <zaelgaming@gmail.com>
2022-09-13 22:53:12 +0200

    level 2 done

---
+++ Assets/Scripts/PlayerMark.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMark : MonoBehaviour
+{
+    public GameObject[] pickupLights;
+    bool activated;
+    bool waiting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+        waiting = false;
+        pickupLights = GameObject.FindGameObjectsWithTag("PickupMark");
+        foreach (GameObject light in pickupLights)
+        {
+            light.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ActivateLights();
+        }
+    }
+
+    void ActivateLights()
+    {
+        foreach (GameObject light in pickupLights)
+        {
+            light.SetActive(!activated);
+        }
+        activated = !activated;
+    }
+}
